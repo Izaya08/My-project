c6f284712fdb4872b60d2643cfe72f5f1b692ee7
Python Isaiah <Isaiahsosee@gmail.com>
2024-09-18 10:50:30 +0800

    playerthree
    uwu

--- Assets/scripts/PlayerThree.cs
+++ Assets/scripts/PlayerThree.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerThree : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI bye;
     public string Name = "Mr. McLovinIt";
     public int Age = 86;
     public string Race = "Doesn't Know";
     public int PhysicalDamage = 30;
     public int Armor = 100;
     public int MagicDamage = 75;
-    public int MagicResistance =  50;
+    public int MagicResistance = 50;
     public float Speed = 0.5f;
-}
+    public void button1press()
+    {
+        bye.text = "Name: " + Name + " \nAge: " + Age + "\nRace: " + Race + " \nPhysicalDamage: " + PhysicalDamage + " \nArmor: " + Armor + " \nMagicDamage: " + MagicDamage + " \nMagicResisrance: " + MagicResistance + " \nSpeed: " + Speed;
+
+    }
+} 
+
