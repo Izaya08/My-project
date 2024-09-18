a0ce54c491326d92e739facb6c8b392c62e09215
11Python(Marcus) <silvergreat0@gmail.com>
2024-09-18 10:18:24 +0800

    playertwo

---
+++ Assets/scripts/PlayerTwo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwo : MonoBehaviour
+{
+	string Name = "Joe Biden";
+	int Age = 81;
+	string Race = "Red, White and Blue American";
+	int Physical_Damage = 20;
+	int Armor = 100;
+	int Magic_Damage = 20;
+	int Magic_Resistance = 5;
+	float Speed = 1.5;
+}
