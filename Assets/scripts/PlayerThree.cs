f2a796e3ec66e3d8a068a604fcc15edf9e701f23
PythonTristan <spleecakes122107@gmail.com>
2024-09-18 10:39:56 +0800

    Player Three Details

---
+++ Assets/scripts/PlayerThree.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerThree : MonoBehaviour
+{
+    public string Name = "Mr. McLovinIt";
+    public int Age = 86;
+    public string Race = "Doesn't Know";
+    public int PhysicalDamage = 30;
+    public int Armor = 100;
+    public int MagicDamage = 75;
+    public int MagicResistance =  50;
+    public float Speed = 0.5f;
+}
