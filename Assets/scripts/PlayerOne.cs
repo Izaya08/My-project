fe7e3572a595d828e9d6c269d16d33381491954c
Python Isaiah <Isaiahsosee@gmail.com>
2024-09-18 10:42:05 +0800

    Player Details for UI
    When pressed, must show.

--- Assets/scripts/PlayerOne.cs
+++ Assets/scripts/PlayerOne.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI hello;
     public string Name = "Will Saucy";
     public int Age = 12;
     public string Race = "African-American";
@@ -12,5 +13,11 @@ public class NewBehaviourScript : MonoBehaviour
     public int Armor = 100;
     public int MagicDamage = 10;
     public int MagicResistance = 50;
-    public int Speed = 100;
+    public float Speed = 100.5f;
+
+    public void button1press()
+    {
+        hello.text = "Name: " + Name + " \nAge: " + Age + "\nRace: " + Race + " \nPhysicalDamage: " + PhysicalDamage + " \nArmor: " + Armor + " \nMagicDamage: " + MagicDamage + " \nMagicResisrance: " + MagicResistance + " \nSpeed: " + Speed; 
+    }
 }
+
--- Assets/scripts/PlayerTwo.cs
+++ Assets/scripts/PlayerTwo.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerTwo : MonoBehaviour
 {
-	string Name = "Joe Biden";
+    [SerializeField] private TextMeshProUGUI Hi;
+    string Name = "Joe Biden";
 	int Age = 81;
 	string Race = "Red, White and Blue American";
-	int Physical_Damage = 20;
+	int PhysicalDamage = 20;
 	int Armor = 100;
-	int Magic_Damage = 20;
-	int Magic_Resistance = 5;
-	float Speed = 1.5;
+	int MagicDamage = 20;
+	int MagicResistance = 5;
+	float Speed = 1.5f;
+
+    public void button1press()
+	{
+        Hi.text = "Name: " + Name + " \nAge: " + Age + "\nRace: " + Race + " \nPhysicalDamage: " + PhysicalDamage + " \nArmor: " + Armor + " \nMagicDamage: " + MagicDamage + " \nMagicResisrance: " + MagicResistance + " \nSpeed: " + Speed;
+    }
 }
+
